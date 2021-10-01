6513fd13f91eca2c5feb7e180a636ab7a3ad4e57
Winand Mathoera <w.a.mathoera@student.tudelft.nl>
2021-10-01 14:07:08 +0200

    Added code for prioritized and constraint checking

--- Aircraft.py
+++ Aircraft.py
@@ -83,23 +83,6 @@ class Aircraft(object):
         xy_from = self.nodes_dict[from_node]["xy_pos"] #xy position of from node
         xy_to = self.nodes_dict[to_node]["xy_pos"] #xy position of to node
         distance_to_move = self.speed*dt #distance to move in this timestep
-
-        # if from_node == to_node:
-        #     print('yes')
-        #
-        #     remaining_path = self.path_to_goal
-        #     self.path_to_goal = remaining_path[1:]
-        #
-        #     new_from_id = self.from_to[1]  # new from node
-        #     new_next_id = self.path_to_goal[0][0]  # new to node
-        #
-        #     if new_from_id != self.from_to[0]:
-        #         self.last_node = self.from_to[0]
-        #
-        #     self.from_to = [new_from_id, new_next_id]  # update new from and to node
-        #     return
-
-        #print('list',t, from_node,to_node,xy_from, xy_to,distance_to_move)
   
         #Update position with rounded values
         x = xy_to[0]-xy_from[0]
