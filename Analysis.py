c307cfeff09eeacdb66ea8ca0a0b0ca8e443f392
Winand Mathoera <w.a.mathoera@student.tudelft.nl>
2021-10-09 12:20:51 +0200

    get xy locations based on node point

--- Analysis.py
+++ Analysis.py
@@ -1,4 +1,91 @@
 import numpy as np
+import os
+import random
+import string
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import time as timer
+import pygame as pg
+from single_agent_planner import calc_heuristics
+from visualization import map_initialization, map_running
+from Aircraft import Aircraft
+from independent import run_independent_planner
+from prioritized import run_prioritized_planner
+from cbs import run_CBS
+
+
+def import_layout(nodes_file, edges_file):
+    """
+    Imports layout information from xlsx files and converts this into dictionaries.
+    INPUT:
+        - nodes_file = xlsx file with node input data
+        - edges_file = xlsx file with edge input data
+    RETURNS:
+        - nodes_dict = dictionary with nodes and node properties
+        - edges_dict = dictionary with edges annd edge properties
+        - start_and_goal_locations = dictionary with node ids for arrival runways, departure runways and gates
+    """
+    gates_xy = []  # lst with (x,y) positions of gates
+    rwy_dep_xy = []  # lst with (x,y) positions of entry points of departure runways
+    rwy_arr_xy = []  # lst with (x,y) positions of exit points of arrival runways
+
+    df_nodes = pd.read_excel(os.getcwd() + "/" + nodes_file)
+    df_edges = pd.read_excel(os.getcwd() + "/" + edges_file)
+
+    # Create nodes_dict from df_nodes
+    nodes_dict = {}
+    for i, row in df_nodes.iterrows():
+        node_properties = {"id": row["id"],
+                           "x_pos": row["x_pos"],
+                           "y_pos": row["y_pos"],
+                           "xy_pos": (row["x_pos"], row["y_pos"]),
+                           "type": row["type"],
+                           "neighbors": set()
+                           }
+        node_id = row["id"]
+        nodes_dict[node_id] = node_properties
+
+        # Add node type
+        if row["type"] == "rwy_d":
+            rwy_dep_xy.append((row["x_pos"], row["y_pos"]))
+        elif row["type"] == "rwy_a":
+            rwy_arr_xy.append((row["x_pos"], row["y_pos"]))
+        elif row["type"] == "gate":
+            gates_xy.append((row["x_pos"], row["y_pos"]))
+
+    # Specify node ids of gates, departure runways and arrival runways in a dict
+    start_and_goal_locations = {"gates": gates_xy,
+                                "dep_rwy": rwy_dep_xy,
+                                "arr_rwy": rwy_arr_xy}
+
+    # Create edges_dict from df_edges
+    edges_dict = {}
+    for i, row in df_edges.iterrows():
+        edge_id = (row["from"], row["to"])
+        from_node = edge_id[0]
+        to_node = edge_id[1]
+        start_end_pos = (nodes_dict[from_node]["xy_pos"], nodes_dict[to_node]["xy_pos"])
+        edge_properties = {"id": edge_id,
+                           "from": row["from"],
+                           "to": row["to"],
+                           "length": row["length"],
+                           "weight": row["length"],
+                           "start_end_pos": start_end_pos
+                           }
+        edges_dict[edge_id] = edge_properties
+
+    # Add neighbor nodes to nodes_dict based on edges between nodes
+    for edge in edges_dict:
+        from_node = edge[0]
+        to_node = edge[1]
+        nodes_dict[from_node]["neighbors"].add(to_node)
+
+    return nodes_dict, edges_dict, start_and_goal_locations
+
+nodes_file = "nodes.xlsx" #xlsx file with for each node: id, x_pos, y_pos, type
+edges_file = "edges.xlsx" #xlsx file with for each edge: from  (node), to (node), length
+nodes_dict, _, _ = import_layout(nodes_file, edges_file)
 
 ac = []
 total = []
@@ -13,5 +100,11 @@ for i in range(0,len(total),2):
 
 #Data is now available as: loc = ac[i][0], time = ac[i][1]
 
-for i in ac:
-    print(i)
+#Nu krijg je de xy locaties van aircraft 1
+a = []
+for loc in ac[0][0]:
+    a.append( nodes_dict[loc]["xy_pos"] )
+print(a)
+
+# for i in ac:
+#     print(i)
